6d93558b235eeb14002beea5c762e8b0176b701a
laura <laura.engleitner@gmail.com>
2023-11-16 13:02:49 +0100

    create response and add contentType

--- src/main/java/org/example/server/http/ContentType.java
+++ src/main/java/org/example/server/http/ContentType.java
@@ -1,7 +1,19 @@
 package org.example.server.http;
 
 public enum ContentType {
-    APPLICATION_JSON,
-    TEXT_PLAIN,
-    TEXT_HTML
+    APPLICATION_JSON("application/json"),
+
+    TEXT_PLAIN("text/plain"),
+
+    TEXT_HTML("text/html");
+
+    private final String mimeType;
+
+    ContentType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
 }
