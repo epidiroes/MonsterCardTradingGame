7af0a3aab65db17346ea2d523068284cd80fc34b
laura <laura.engleitner@gmail.com>
2023-11-28 12:30:53 +0100

    created toObject() and json() in Controller

--- src/main/java/org/example/apps/mctg/controller/Controller.java
+++ src/main/java/org/example/apps/mctg/controller/Controller.java
@@ -1,5 +1,55 @@
 package org.example.apps.mctg.controller;
 
-public class Controller {
-    // TODO
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.server.http.HttpContentType;
+import org.example.server.http.HttpStatus;
+import org.example.server.http.Request;
+import org.example.server.http.Response;
+
+public abstract class Controller {
+
+    public abstract boolean supports(String route);
+
+    public abstract Response handle(Request request);
+
+    protected Response status(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(HttpContentType.APPLICATION_JSON);
+        response.setBody("{ \"error\": \""+ httpStatus.getMessage() + "\"}");
+
+        return response;
+    }
+
+
+    protected <T> T toObject(String requestBody, Class<T> targetClass) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        T object = null;
+        try {
+            object = objectMapper.readValue(requestBody, targetClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return object;
+    }
+
+    protected <T> Response json(T object) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String objectJson = null;
+        try {
+            objectJson = objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        Response response = new Response();
+        response.setStatus(HttpStatus.CREATED);
+        response.setContentType(HttpContentType.APPLICATION_JSON);
+        response.setBody(objectJson);
+
+        return response;
+    }
+
+    // THOUGHT: more functionality e.g. ok(), json(), etc
 }
