54f4c7fee289a5e9b6b8514bfcadb5ede7e6f044
laura <laura.engleitner@gmail.com>
2023-12-29 15:08:05 +0100

    create CardRepository

--- src/main/java/org/example/apps/mctg/repository/CardRepository.java
+++ src/main/java/org/example/apps/mctg/repository/CardRepository.java
@@ -1,5 +1,30 @@
 package org.example.apps.mctg.repository;
 
+import org.example.apps.mctg.database.Database;
+import org.example.apps.mctg.entity.Card;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CardRepository {
-    // TODO
+    private final Database database = new Database();
+
+    public Card save(Card card) {
+        String SAVE_SQL = "INSERT INTO cards(id, name, damage) VALUES (?, ?, ?)";
+        try (
+                Connection con = database.getConnection();
+                PreparedStatement stmt = con.prepareStatement(SAVE_SQL);
+        ) {
+            stmt.setString(1, card.getId());
+            stmt.setString(2, card.getName());
+            stmt.setInt(3, card.getDamage());
+
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return card;
+    }
 }
