6ad34ca0df649df4bb823ee75f308917765c87e5
laura <laura.engleitner@gmail.com>
2023-12-30 13:13:52 +0100

    implement package logic

--- src/main/java/org/example/apps/mctg/controller/PackageController.java
+++ src/main/java/org/example/apps/mctg/controller/PackageController.java
@@ -1,5 +1,46 @@
 package org.example.apps.mctg.controller;
 
-public class PackageController {
-    // TODO
+import org.example.apps.mctg.entity.Card;
+import org.example.apps.mctg.entity.Package;
+import org.example.apps.mctg.repository.CardRepository;
+import org.example.apps.mctg.repository.PackageRepository;
+import org.example.apps.mctg.repository.UserRepository;
+import org.example.apps.mctg.service.PackageService;
+import org.example.server.http.HttpStatus;
+import org.example.server.http.Request;
+import org.example.server.http.Response;
+
+import java.util.List;
+
+public class PackageController extends Controller {
+    private final PackageService packageService;
+    public PackageController() {
+        this.packageService = new PackageService(new PackageRepository(), new CardRepository(), new UserRepository());
+    }
+
+    @Override
+    public boolean supports(String route) {
+        return route.startsWith("/packages");
+    }
+
+    @Override
+    public Response handle(Request request) {
+
+        if (request.getRoute().equals("/packages")) {
+            if (request.getMethod().equals("POST")) {
+                return create(request);
+            }
+            return status(HttpStatus.METHOD_NOT_ALLOWED);
+        }
+        return null;
+    }
+
+    public Response create(Request request) {
+        List<Card> cards = toObjects(request, Card.class);
+        Package p = packageService.save(request, cards);
+        if (p == null) {
+            return statusMessage(HttpStatus.BAD_REQUEST, "Could not create package (no authorization or cards already exist");
+        }
+        return ok(json(p));
+    }
 }
---
+++ src/main/java/org/example/apps/mctg/service/PackageService.java
@@ -0,0 +1,51 @@
+package org.example.apps.mctg.service;
+
+import org.example.apps.mctg.entity.Card;
+import org.example.apps.mctg.entity.User;
+import org.example.apps.mctg.repository.CardRepository;
+import org.example.apps.mctg.repository.PackageRepository;
+import org.example.apps.mctg.entity.Package;
+import org.example.apps.mctg.repository.UserRepository;
+import org.example.server.http.Request;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class PackageService {
+    private final PackageRepository packageRepository;
+    private final CardRepository cardRepository;
+    private final UserRepository userRepository;
+    public PackageService(PackageRepository packageRepository, CardRepository cardRepository, UserRepository userRepository) {
+        this.packageRepository = packageRepository;
+        this.cardRepository = cardRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Package save(Request request, List<Card> cards) {
+        // check authorization
+        if (!Objects.equals(request.getAuthorization(), "Bearer admin-mtcgToken")) {
+            return null;
+        }
+        Optional<User> user = userRepository.find("admin");
+        if (user.isEmpty()) {
+            return null;
+        }
+        User admin = user.get();
+
+        // save cards
+        if(!saveAll(cards)) {
+            return null;
+        }
+        return packageRepository.save(admin, cards);
+    }
+
+    public boolean saveAll(List<Card> cards) {
+        for (Card card : cards) {
+            if (!cardRepository.save(card)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
