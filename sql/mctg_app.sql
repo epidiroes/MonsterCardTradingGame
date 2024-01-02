a5e6b4d7c1bb53357aade9169051b15d7994692e
laura <laura.engleitner@gmail.com>
2024-01-02 16:21:32 +0100

    create bio and edit user table

--- sql/mctg_app.sql
+++ sql/mctg_app.sql
@@ -7,7 +7,10 @@ CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    coins INT NOT NULL
+    coins INT NOT NULL,
+    name VARCHAR(255),
+    bio VARCHAR(255),
+    image VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS cards (
---
+++ src/main/java/org/example/apps/mctg/dto/Bio.java
@@ -0,0 +1,38 @@
+package org.example.apps.mctg.dto;
+
+public class Bio {
+    private String name;
+    private String bio;
+    private String image;
+
+    Bio() {}
+    Bio(String name, String bio, String image) {
+        this.name = name;
+        this.bio = bio;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
