3edcf8824740d0a65f7630e2ee9ba74778970332
laura <laura.engleitner@gmail.com>
2024-01-08 15:52:19 +0100

    implement BattleController and Service

--- src/main/java/org/example/apps/mctg/controller/BattleController.java
+++ src/main/java/org/example/apps/mctg/controller/BattleController.java
@@ -1,5 +1,37 @@
 package org.example.apps.mctg.controller;
 
-public class BattleController {
-    // TODO
+import org.example.apps.mctg.logic.BattleLogic;
+import org.example.apps.mctg.repository.BattleRepository;
+import org.example.apps.mctg.repository.CardRepository;
+import org.example.apps.mctg.repository.DeckRepository;
+import org.example.apps.mctg.repository.UserRepository;
+import org.example.apps.mctg.service.BattleService;
+import org.example.server.http.HttpStatus;
+import org.example.server.http.Request;
+import org.example.server.http.Response;
+
+public class BattleController extends Controller {
+    private final BattleService battleService;
+    public BattleController() {
+        this.battleService = new BattleService(new UserRepository(), new BattleRepository(), new BattleLogic());
+    }
+    @Override
+    public boolean supports(String route) {
+        return route.startsWith("/battles");
+    }
+
+    @Override
+    public Response handle(Request request) {
+        if (request.getRoute().equals("/cards")) {
+            if (request.getMethod().equals("GET")) {
+                return battle(request);
+            }
+            return status(HttpStatus.METHOD_NOT_ALLOWED);
+        }
+        return null;
+    }
+
+    private Response battle(Request request) {
+        return ok(battleService.battle(request));
+    }
 }
--- src/main/java/org/example/apps/mctg/service/BattleService.java
+++ src/main/java/org/example/apps/mctg/service/BattleService.java
@@ -1,5 +1,52 @@
 package org.example.apps.mctg.service;
 
+import org.example.apps.mctg.entity.Battle;
+import org.example.apps.mctg.entity.User;
+import org.example.apps.mctg.logic.BattleLogic;
+import org.example.apps.mctg.repository.BattleRepository;
+import org.example.apps.mctg.repository.CardRepository;
+import org.example.apps.mctg.repository.DeckRepository;
+import org.example.apps.mctg.repository.UserRepository;
+import org.example.server.http.Request;
+
+import java.util.Objects;
+import java.util.Optional;
+
 public class BattleService {
-    // TODO
+    private final UserRepository userRepository;
+    private final BattleRepository battleRepository;
+    private final BattleLogic battleLogic;
+    public BattleService(UserRepository userRepository, BattleRepository battleRepository, BattleLogic battleLogic) {
+        this.userRepository = userRepository;
+        this.battleRepository = battleRepository;
+        this.battleLogic = battleLogic;
+    }
+    public String battle(Request request) {
+        // Authentication
+        String authorization = request.getAuthorization();
+        if (Objects.equals(authorization, "")) {
+            return null;
+        }
+        String name = authorization.substring(authorization.indexOf(" ") + 1, authorization.indexOf("-", authorization.indexOf(" ") + 1));
+        Optional<User> userOptional = userRepository.find(name);
+        if (userOptional.isEmpty()) {
+            return null;
+        }
+        User user = userOptional.get();
+
+        // Find open battle or open new battle
+        Optional<Battle> openBattle = battleRepository.findOpenBattle();
+        if (openBattle.isEmpty()) {
+            Battle battle = battleRepository.openBattle(user);
+            return awaitBattle(battle);
+        } else {
+            Battle battle = openBattle.get();
+            return battleLogic.battle(battle.getPlayer1(), user.getId());
+        }
+    }
+
+    private String awaitBattle(Battle battle) {
+        // TODO
+        return null;
+    }
 }
