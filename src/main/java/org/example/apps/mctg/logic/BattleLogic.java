9adbc69caa332bcd47eba8dae336f5505fbd1de8
laura <laura.engleitner@gmail.com>
2024-01-08 15:51:55 +0100

    add BattleLogic and Repository with empty methods

---
+++ src/main/java/org/example/apps/mctg/logic/BattleLogic.java
@@ -0,0 +1,10 @@
+package org.example.apps.mctg.logic;
+
+import org.example.apps.mctg.entity.User;
+
+public class BattleLogic {
+    public String battle(String user1Id, String user2Id) {
+        // TODO
+        return null;
+    }
+}
---
+++ src/main/java/org/example/apps/mctg/repository/BattleRepository.java
@@ -0,0 +1,18 @@
+package org.example.apps.mctg.repository;
+
+import org.example.apps.mctg.entity.Battle;
+import org.example.apps.mctg.entity.User;
+
+import java.util.Optional;
+
+public class BattleRepository {
+    public Optional<Battle> findOpenBattle() {
+        // TODO
+        return Optional.empty();
+    }
+
+    public Battle openBattle(User user) {
+        // TODO
+        return null;
+    }
+}
