c913a0a53cb738d7a8f00137d565b500aa92514e
laura <laura.engleitner@gmail.com>
2024-01-13 16:39:07 +0100

    implement methods for TradingsController

--- src/main/java/org/example/apps/mctg/controller/TradingsController.java
+++ src/main/java/org/example/apps/mctg/controller/TradingsController.java
@@ -1,8 +1,15 @@
 package org.example.apps.mctg.controller;
 
+import org.example.apps.mctg.entity.Card;
+import org.example.apps.mctg.entity.Trade;
+import org.example.apps.mctg.entity.User;
+import org.example.apps.mctg.repository.CardRepository;
+import org.example.apps.mctg.repository.TradingsRepository;
 import org.example.apps.mctg.repository.UserRepository;
 import org.example.apps.mctg.service.AuthorizationService;
+import org.example.apps.mctg.service.CardService;
 import org.example.apps.mctg.service.TradingsService;
+import org.example.server.http.HttpException;
 import org.example.server.http.HttpStatus;
 import org.example.server.http.Request;
 import org.example.server.http.Response;
@@ -10,9 +17,11 @@ import org.example.server.http.Response;
 public class TradingsController extends Controller {
     private final AuthorizationService authorizationService;
     private final TradingsService tradingsService;
+    private final CardService cardService;
     public TradingsController() {
         this.authorizationService = new AuthorizationService(new UserRepository());
-        this.tradingsService = new TradingsService();
+        this.tradingsService = new TradingsService(new TradingsRepository());
+        this.cardService = new CardService(new CardRepository(), new UserRepository());
     }
 
     @Override
@@ -40,18 +49,46 @@ public class TradingsController extends Controller {
     }
 
     private Response readAll(Request request) {
-        return ok("TODO");
+        authorizationService.checkAuthorization(request.getAuthorization());
+        return ok(json(tradingsService.readAll()));
     }
 
     private Response create(Request request) {
-        return ok("TODO");
+        User user = authorizationService.authorizedUser(request.getAuthorization());
+        Trade trade = toObject(request, Trade.class);
+        trade.setUserId(user.getId());
+        return created(json(tradingsService.create(trade)));
     }
 
     private Response trade(Request request) {
-        return ok("TODO");
+        User user = authorizationService.authorizedUser(request.getAuthorization());
+        String id = request.getRoute().substring(request.getRoute().lastIndexOf('/') + 1);
+        Trade trade = tradingsService.find(id);
+        if (user.getId().equals(trade.getUserId())) {
+            throw new HttpException(HttpStatus.UNAUTHORIZED, "trying to trade with yourself");
+        }
+        String cardId = toObject(request, String.class);
+        Card card = cardService.findById(cardId);
+        if (
+                trade.getType().equals("monster") && card.isSpell() ||
+                trade.getType().equals("spell") && !card.isSpell() ||
+                trade.getMinimumDamage() > card.getDamage()
+        ) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "offered card doesn't fulfill trading requirements");
+        }
+        cardService.updateUser(card, user);
+        tradingsService.delete(id);
+        return ok("Trade successful");
     }
 
     private Response delete(Request request) {
-        return ok("TODO");
+        User user = authorizationService.authorizedUser(request.getAuthorization());
+        String id = request.getRoute().substring(request.getRoute().lastIndexOf('/') + 1);
+        Trade trade = tradingsService.find(id);
+        if (!user.getId().equals(trade.getUserId())) {
+            throw new HttpException(HttpStatus.UNAUTHORIZED, "trade doesn't belong to user trying to delete it");
+        }
+        tradingsService.delete(id);
+        return ok(json(trade));
     }
 }
