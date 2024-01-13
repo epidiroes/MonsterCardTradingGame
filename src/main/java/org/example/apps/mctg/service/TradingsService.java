f1988a70e20d0ab127d410d94e51294811653bde
laura <laura.engleitner@gmail.com>
2024-01-13 16:37:54 +0100

    create methods for TradingsService

--- src/main/java/org/example/apps/mctg/service/TradingsService.java
+++ src/main/java/org/example/apps/mctg/service/TradingsService.java
@@ -1,5 +1,31 @@
 package org.example.apps.mctg.service;
 
+import org.example.apps.mctg.entity.Trade;
+import org.example.apps.mctg.repository.TradingsRepository;
+
+import java.util.List;
+
 public class TradingsService {
-    // TODO
+    private TradingsRepository tradingsRepository;
+    public TradingsService(TradingsRepository tradingsRepository) {
+        this.tradingsRepository = tradingsRepository;
+    }
+
+    public List<Trade> readAll() {
+        return tradingsRepository.findAll();
+    }
+
+    public Trade find(String id) {
+        return tradingsRepository.findById(id);
+    }
+
+    public Trade create(Trade trade) {
+        return tradingsRepository.save(trade);
+    }
+
+    public void delete(String id) {
+        tradingsRepository.delete(id);
+    }
+
+
 }
