540fc18b544584deb266d6705bdee80f2833c4cd
laura <laura.engleitner@gmail.com>
2024-01-13 16:38:35 +0100

    add methods findById() and updateUser() to CardService

--- src/main/java/org/example/apps/mctg/service/CardService.java
+++ src/main/java/org/example/apps/mctg/service/CardService.java
@@ -4,6 +4,8 @@ import org.example.apps.mctg.entity.Card;
 import org.example.apps.mctg.entity.User;
 import org.example.apps.mctg.repository.CardRepository;
 import org.example.apps.mctg.repository.UserRepository;
+import org.example.server.http.HttpException;
+import org.example.server.http.HttpStatus;
 import org.example.server.http.Request;
 
 import java.util.List;
@@ -18,6 +20,13 @@ public class CardService {
         this.userRepository = userRepository;
     }
 
+    public Card findById(String id) {
+        Optional<Card> optionalCard = cardRepository.findById(id);
+        if (optionalCard.isEmpty()) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "card does not exist");
+        }
+        return optionalCard.get();
+    }
     public List<Card> findAll(Request request) {
         String authorization = request.getAuthorization();
         if (Objects.equals(authorization, "")) {
@@ -31,5 +40,9 @@ public class CardService {
         User user = userOptional.get();
 
         return cardRepository.findAll(user);
+    }
+
+    public void updateUser(Card card, User user) {
+        cardRepository.updateUserId(card, user);
     }
 }
